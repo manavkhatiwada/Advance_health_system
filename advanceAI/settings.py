165bdbd55109b2f8456993a8d43f323f0edb08cc
Manav_khatiwada <manav66665555@gmail.com>
2026-04-03 17:15:58 +0545

    created blog section with seralizer

--- advanceAI/settings.py
+++ advanceAI/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'chatbot',
+    'blogs',
     'student',
     'api',
     'rest_framework',
---
+++ blogs/seralizers.py
@@ -0,0 +1,14 @@
+from rest_framework import serializers
+from .models import Blog, Comment
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    class Meta:
+        Model = Comment
+        fields = '__all__'
+
+
+class BlogSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Blog
+        fields = '__all__'
